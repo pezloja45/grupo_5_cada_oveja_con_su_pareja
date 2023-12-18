9faba079d5ba01863cf95d59bf3eb98811918ba2
Hugo <martin.romero.hugo@iescamas.es>
2023-12-18 18:38:04 +0100

    Arreglo de errores. Cambios en el código: Genera el tablero y genera caracteres únicos

--- PROYECTOOVEJASV2/src/proyectoovejas/proyecto_ovejas.java
+++ PROYECTOOVEJASV2/src/proyectoovejas/proyecto_ovejas.java
@@ -1,5 +1,6 @@
 package proyectoovejas;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class proyecto_ovejas {
@@ -7,18 +8,38 @@ public class proyecto_ovejas {
 	public static void main(String[] args) {
 		var sc = new Scanner(System.in);
 
-		System.out.println("Introduzca la dificultad (2,4,6)");
+		System.out.println("Introduzca la dificultad (2, 4, 6)");
 		int dificultad = sc.nextInt();
+		
 		//Este metodo valida si la dificultad introducida por el usuario es valida y si no pide otra
 		while (selectorDificultad(dificultad) == false) {
 			System.out.println("Dificultad no válida seleccione un valor válido (2, 4, 6)");
 			dificultad = sc.nextInt();
 		}
+	
+		char[][] matriz = new char[dificultad][dificultad];
+		
+		//Este bucle asigna valores unicos por parejas a la matriz
+		for (int i = 0; i < dificultad; i++) {
+			for (int j = 0; j < dificultad; j += 2) {
+				char caracter = generarCaracterUnico(matriz);
+				matriz[i][j] = caracter;
+				matriz[i][j + 1] = caracter;
+			}
+		}
+		
 	}
 
-		
-	//Este metodo retorna true si el numero que le pides al usuario es 2, 4 o 6
+	public static char generarCaracterUnico(char[][] matriz) {
+		//Este metodo genera un caracter aleatorio de la a hasta 
+		Random random = new Random();
+		char caracter;
+		caracter = (char) (random.nextInt(25) + 'a');
+		return caracter;
+	}	
+	
 	public static boolean selectorDificultad(int dificultad) {
+		//Este metodo retorna true si el numero que le pides al usuario es 2, 4 o 6
 		return dificultad == 2 || dificultad == 4 || dificultad == 6;
 	}
 }
